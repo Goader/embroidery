ce544025f2aa9afac98dc4c13f6c8b4b3e596166
Goader <goader-info@users.noreply.github.com>
2020-08-23 19:56:35 +0300

    unsimilar images selecting added

---
+++ scripts/image_distance.py
@@ -0,0 +1,55 @@
+from scipy.fftpack import dct
+from cv2 import cv2
+from visual import save_image
+from PIL import Image
+import gmpy2
+import os
+import numpy as np
+
+
+def eucl_dist(img1, img2):
+    delta = img1 - img2
+    return np.sum(np.square(delta))
+
+
+def phash(img, hash_size=8, factor=4):
+    img = np.array(img, dtype=np.uint8)
+    img = Image.fromarray(img)
+    image_size = hash_size * factor
+    img.convert('L').resize((image_size, image_size), Image.ANTIALIAS)
+    img = np.asarray(img)
+    dct_ = dct(dct(img, axis=0), axis=1)
+    # dct_ = dct_[:hash_size, :hash_size]
+    med = np.median(dct_)
+    diff = dct_ > med
+    return sum((1 << i) * int(el) for i, el in enumerate(diff.flatten()))
+
+
+icons = []
+imgs = []
+for filename in os.listdir('../icons/'):
+    if filename.endswith('.jpg'):
+        imgs.append(np.array(cv2.imread('../icons/' + filename)))
+
+for idx, filename in enumerate(os.listdir('../icons/')):
+    if filename.endswith('.jpg'):
+        # distance = sum(eucl_dist(imgs[idx], other_img) for other_img in imgs)
+        distance = 0
+
+        icons.append({'filename': filename,
+                      'ord': filename[:filename.rfind('.')],
+                      'distance': distance,
+                      'image': imgs[idx],
+                      'phash': phash(imgs[idx])})
+
+for icon1 in icons:
+    distance = 0
+    for icon2 in icons:
+        distance += gmpy2.popcount(int(np.bitwise_xor(icon1['phash'], icon2['phash'])))
+    icon1['distance'] = distance
+
+icons.sort(key=lambda x: x['distance'])
+icons = icons[::-1]
+
+for i, icon in enumerate(icons, 1000):
+    save_image(icon['image'], f'../icons/_{i}_{icon["ord"]}.jpg')
--- scripts/unicode_transform.py
+++ scripts/unicode_transform.py
@@ -1,40 +1,39 @@
 from __future__ import unicode_literals
-from scripts.visual import save_image
+from visual import save_image
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-# font = ImageFont.load(r'C:\Users\Hyperbook\AppData\Local\Microsoft\Windows\Fonts\unifont-13.0.03.pil')
-font = ImageFont.truetype(r'fonts\ARIALUNI.TTF', size=26)
-# font = ImageFont.truetype(r'C:\Windows\Fonts\Arial\arial.ttf', size=32)
 
-# def post_processing(img):
-#     pixels = np.array(img.getdata())
-#     for idx, pixel in enumerate(pixels):
-#         for c in range(3):
-#             pixels[idx][c] = (pixel[c]//128) * 255 
+h = w = 32
+font = ImageFont.truetype(r'..\fonts\ARIALUNI.TTF', size=26)
 
-#     pixels[:, :] = (pixels[:, :]//128) * 255 doest it work? this is the question :)
 
-#     pixels = np.reshape(pixels, (img.height, img.width, 3))
-
-#     return pixels
-
-def transform_unicode(unicode_symb):
-    h = w = 32
+def transform_unicode(unicode_symb, return_array=False):
     img = Image.new('RGB', (w, h), (255, 255, 255))
     new_img = ImageDraw.Draw(img)
+
     icon_w, icon_h = new_img.textsize(unicode_symb, font)
     coords = ((w-icon_w) // 2, (h-icon_h) // 2 - 4)
     new_img.text(coords, unicode_symb, fill=(0, 0, 0), font=font)
-    # new_img.bitmap((3, 3), font.getmask(unicode_symb))
-    # img.paste(img, mask=font.getmask(unicode_symb))
-    # print(font.getmask(unicode_symb))
-    # img = post_processing(img)
+
     img = np.array(img.getdata())
     img = np.reshape(img, (h, w, 3))
-    filepath = 'icons/' + str(ord(unicode_symb)) + '.jpg'
+    if return_array:
+        return img
+    if np.all(img == no_symb):
+        return
+    filepath = '../icons/' + str(ord(unicode_symb)) + '.jpg'
     save_image(img, filepath)
 
-transform_unicode('◐')
+
+no_symb = transform_unicode(chr(0), return_array=True)
+
+
+def get_icons(start=0, end=1114111, step=1):
+    for i in range(start, end, step):
+        transform_unicode(chr(i))
+
+
+get_icons(start=1000, end=10000)
 
 # ⇔ ▰ ▲ ◐ ◈ ◉ ◤ ▣ ▧
